bb87da2e515c0ec71126690f32b0305555dc500c
Switch Training Team <tarmz98@gmail.com>
2026-03-11 10:04:53 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -28,19 +28,18 @@ with col1:
     st.link_button("iPad", "https://ssc2026februaryquest-hjxkt9mlcfccphmmuxetgw.streamlit.app", use_container_width=True)
 
 with col2:
-    st.markdown("<h2 style='text-align: center;'>🥈</h2>", unsafe_allow_html=True)
-    st.markdown("<h3 style='text-align: center;'>Adventurer</h3>", unsafe_allow_html=True)
-    st.markdown("<p style='text-align: center;'>Bravery and Skill</p>", unsafe_allow_html=True)
-    st.markdown("<p style='text-align: center;'>Go to Level 2</p>", unsafe_allow_html=True)
-    
-    st.link_button("iPhone and Apple Watch", "https://levelssc2026januaryquest-chdvfgsnwh3vfajfeaj9pe.streamlit.app", use_container_width=True)
-    st.link_button("iPad", "https://ssc2026februaryquest-hjxkt9mlcfccphmmuxetgw.streamlit.app", use_container_width=True)
+    st.header("🥈")
+    st.subheader("Adventurer")
+    st.write("Bravery and Skill")
+    st.write("Go to Level 2")
+    st.link_button("Role-play", "", use_container_width=True)
 
 with col3:
     st.header("🥇")
     st.subheader("Master")
     st.write("Legacy and Ultimate Challenge")
-    st.link_button("Go to Level 3", "", use_container_width=True)
+    st.write("Go to Level 3")
+    st.link_button("Short Video", "", use_container_width=True)
 
 st.markdown("---")
 st.caption("© 2026 Switch Staff Challenge | Insight Team")
