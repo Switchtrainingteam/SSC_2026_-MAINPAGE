5ac2cce0314219d97a376292ef63e48519cc97c5
Switch Training Team <tarmz98@gmail.com>
2026-04-23 11:03:30 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -26,7 +26,7 @@ with col1:
     st.write("Go to Level 1")
     st.link_button("iPhone and Apple Watch", "https://levelssc2026januaryquest-chdvfgsnwh3vfajfeaj9pe.streamlit.app", use_container_width=True)
     st.link_button("iPad", "https://ssc2026februaryquest-hjxkt9mlcfccphmmuxetgw.streamlit.app", use_container_width=True)
-    st.link_button("Mac", "https://ssc2026-mainpage-qsqyxlxdaswz2zebu28mfq.streamlit.app", use_container_width=True)
+    st.link_button("Mac", "https://levelssc2026aprilquest-l2vqlcqdsadg8r6bmgqyei.streamlit.app", use_container_width=True)
 
 with col2:
     st.header("🥈")
